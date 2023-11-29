c3e03eee948b352618c26b3a2fc66af6ea7dd559
vaibhavjain2609 <vaibhavjain@protonmail.com>
2023-11-28 17:37:21 -0800

    feat: Connected DB + Installed Flask template

---
+++ insta485/config.py
@@ -0,0 +1,20 @@
+"""Insta485 development configuration."""
+
+import pathlib
+
+# Root of this application, useful if it doesn't occupy an entire domain
+APPLICATION_ROOT = '/'
+
+# Secret key for encrypting cookies
+SECRET_KEY = b'}\x08\x1f}V}\x8a\x1c\xe5W\xac\x05\xd1z\x05&6\x87\xd5\xdf\x1d\\\xfd\xb3'
+SESSION_COOKIE_NAME = 'login'
+
+# File Upload to var/uploads/
+INSTA485_ROOT = pathlib.Path(__file__).resolve().parent.parent
+UPLOAD_FOLDER = INSTA485_ROOT/'var'/'uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+# Database file is var/insta485.sqlite3
+DATABASE_FILENAME = INSTA485_ROOT/'var'/'insta485.sqlite3'
+
